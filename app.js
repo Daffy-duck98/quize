d978c998b87c62c3b4fd0c84fe6dbe44bcdc8dbe
Vasily <ololol2113@mail.ru>
2023-01-19 18:09:09 +0300

    serverUP

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 require('@babel/register');
-
 const express = require('express');
 
 const app = express();
+
+const PORT = 3000;
+
+// const mainRoute = require('./routes/main.routes');
+// const citatesRoute = require('./routes/citates.routes');
+// const authRoute = require('./routes/auth.routes');
+
+// app.use('/', mainRoute);
+// app.use('/citates', citatesRoute);
+// app.use('/auth', authRoute);
+
+app.listen(PORT, () => {
+    console.log(`Порт ЗАПУЩЕН!!! ${PORT}`);
+});
